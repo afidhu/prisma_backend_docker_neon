a1a3db5462629fc68258fc38bf31a7d2e782480c
afidhu <afidhumatata81@gmail.com>
2025-12-20 17:08:47 +0300

    import express from "express";

--- server/api/index.ts
+++
@@ -1,5 +0,0 @@
-
-import serverless from "serverless-http";
-import app from "../src/index.ts";
-
-export default serverless(app);
--- server/src/index.ts
+++ server/src/index.ts
@@ -22,13 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// ✅ ONLY listen when running locally
-if (process.env.NODE_ENV !== "production") {
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-  });
-}
 
 // Serve static files from the "uploads" directory Also 
 // make sure to create the uploads folder in the root directory For Accessing images in the browser
@@ -41,13 +34,9 @@ app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use('/post', postRoute)
 
-
-
-export default app;
-
     
-// app.listen(port, () => {
-//   console.log(`[server]: Server is running at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
 
 // export default serverless(app);
