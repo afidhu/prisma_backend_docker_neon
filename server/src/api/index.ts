19b284ac87132160f6abf408a389bcf8c856c8c2
afidhu <afidhumatata81@gmail.com>
2025-12-20 16:37:05 +0300

    addedd new file:   server/src/api/index.ts modified:   server/src/index.ts

---
+++ server/src/api/index.ts
@@ -0,0 +1,5 @@
+
+import serverless from "serverless-http";
+import app from "../index.ts";
+
+export default serverless(app);
--- server/src/index.ts
+++ server/src/index.ts
@@ -21,10 +21,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-export default function handler(req: Request, res: Response) {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    if(req.method==='OPTIONS') return res.status(200).end();
-    res.json({message:'API working'})
+
+// ✅ ONLY listen when running locally
+if (process.env.NODE_ENV !== "production") {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
 }
 
 // Serve static files from the "uploads" directory Also 
@@ -40,11 +43,11 @@ app.use('/post', postRoute)
 
 
 
-
+export default app;
 
     
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`[server]: Server is running at http://localhost:${port}`);
+// });
 
 // export default serverless(app);
