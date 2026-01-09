a5482f93d485809b2babaf3a4eaac5b72d712331
afidhu <afidhumatata81@gmail.com>
2026-01-10 00:34:44 +0300

    dist/index.js modified

---
+++ server/dist/controllers/postController.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addPostData = exports.getAllpost = void 0;
+const index_ts_1 = require("../index.ts");
+const HOST = "https://prisma-backend-docker-neon.onrender.com";
+const getAllpost = async (req, resp) => {
+    const posts = await index_ts_1.prisma.post.findMany();
+    ////This Code is to map the image URL to be accessible via HTTP
+    const mappedPosts = posts.map(posts => ({ ...posts, imageUrl: `${HOST}/uploads/${posts.imageUrl}` }));
+    return resp.status(200).json(mappedPosts);
+};
+exports.getAllpost = getAllpost;
+const addPostData = async (req, resp) => {
+    try {
+        const file = req.file; // multer adds this
+        if (!file) {
+            return resp.status(400).json({ message: "No file uploaded" });
+        }
+        const posts = await index_ts_1.prisma.post.create({
+            data: {
+                title: req.body.title,
+                content: req.body.content,
+                imageUrl: file.filename,
+                authorId: Number(req.body.authorId)
+            }
+        });
+        console.log(posts);
+        return resp.status(201).json(posts);
+    }
+    catch (error) {
+        console.log(error.message);
+        return resp.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+exports.addPostData = addPostData;
--- server/dist/index.js
+++ server/dist/index.js
@@ -1,30 +1,37 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { PrismaClient } from "./generated/prisma/client.js";
-import path from "path";
-import postRoute from './src/routes/postRoute.js';
-import cors from 'cors';
-import { fileURLToPath } from 'url';
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.prisma = void 0;
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const client_ts_1 = require("../generated/prisma/client.ts");
+const path_1 = __importDefault(require("path"));
+const postRoute_ts_1 = __importDefault(require("./routes/postRoute.ts"));
+const serverless_http_1 = __importDefault(require("serverless-http"));
+const cors_1 = __importDefault(require("cors"));
+const url_1 = require("url");
 // import path from 'path/win32';
-dotenv.config(); // Load environment variables
-const app = express();
+dotenv_1.default.config(); // Load environment variables
+const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-export const prisma = new PrismaClient();
+exports.prisma = new client_ts_1.PrismaClient();
 // Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 // Serve static files from the "uploads" directory Also 
 // make sure to create the uploads folder in the root directory For Accessing images in the browser
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-app.use('/post', postRoute);
+const __filename = (0, url_1.fileURLToPath)(import.meta.url);
+const __dirname = path_1.default.dirname(__filename);
+app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "../uploads")));
+app.use('/post', postRoute_ts_1.default);
 app.get('/', (req, res) => {
     res.send('https://prisma-backend-docker-neon-6vez.vercel.app/post');
 });
 // app.listen(port, () => {
 //   console.log(`[server]: Server is running at http://localhost:${port}`);
 // });
-export default app;
-// export default serverless(app);
+// export default app;
+exports.default = (0, serverless_http_1.default)(app);
